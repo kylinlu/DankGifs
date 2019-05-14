7e8c5cf83113b9c2d4e851867a69a421c47ad268
䂊汐 <28.juxi.lu@gmail.com>
2019-05-13 23:27:42 -0400

    basic Clarifai predict

---
+++ index.js
@@ -0,0 +1,13 @@
+const Clarifai = require('clarifai');
+
+// Instantiate a new Clarifai app by passing in your API key.
+const app = new Clarifai.App({apiKey: 'YOUR_API_KEY'});
+
+// Predict the contents of an image by passing in a URL.
+app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg')
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.log(err);
+  });
