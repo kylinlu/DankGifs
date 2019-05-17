2bfe10ec7f02b8cc1bcb9e05de4c2ef95f372be8
䂊汐 <28.juxi.lu@gmail.com>
2019-05-16 22:26:48 -0400

    parse/extract out #1 highest probable image keyword

--- server/src/predict.js
+++ server/src/predict.js
@@ -7,7 +7,7 @@ const app = new Clarifai.App({apiKey: process.env.API_KEY});
 // Predict the contents of an image by passing in a URL.
 app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg')
   .then(response => {
-    console.log(JSON.stringify(response));
+    console.log(JSON.stringify(response.outputs[0]["data"]["concepts"][0]["name"]));
   })
   .catch(err => {
     console.log(err);
