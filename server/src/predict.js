e8e3d42e87fe7631b3b3cd1f78ec3a3cd03e34f0
䂊汐 <28.juxi.lu@gmail.com>
2019-05-16 22:03:51 -0400

    stringify json reponse

--- server/src/predict.js
+++ server/src/predict.js
@@ -7,7 +7,7 @@ const app = new Clarifai.App({apiKey: process.env.API_KEY});
 // Predict the contents of an image by passing in a URL.
 app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg')
   .then(response => {
-    console.log(response);
+    console.log(JSON.stringify(response));
   })
   .catch(err => {
     console.log(err);
