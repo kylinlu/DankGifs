74992a30f86c22dcf8c4fecb32d33b1adaf382ef
䂊汐 <28.juxi.lu@gmail.com>
2019-05-18 18:14:25 -0400

    functional giphy API search

--- server/src/predict.js
+++ server/src/predict.js
@@ -1,19 +1,33 @@
 require('dotenv').config();
 const Clarifai = require('clarifai');
+var GphApiClient = require('giphy-js-sdk-core');
+client = GphApiClient(process.env.GIPHY_KEY);
 
 // Instantiate a new Clarifai app by passing in your API key.
-const app = new Clarifai.App({apiKey: process.env.API_KEY});
+const app = new Clarifai.App({apiKey: process.env.CLARIFAI_KEY});
+
+var guess
 
 // Predict the contents of an image by passing in a URL.
 app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg')
   .then(response => {
-    console.log(JSON.stringify(response.outputs[0]["data"]["concepts"][0]["name"]));
-    console.log(JSON.stringify(response.outputs[0]["data"]["concepts"][0]["value"]));
+    guess= JSON.stringify(response.outputs[0]["data"]["concepts"][0]["name"]);
+    guessAccuracy = JSON.stringify(response.outputs[0]["data"]["concepts"][0]["value"]);
   })
   .catch(err => {
     console.log(err);
   });
-
+  
+  client.search('gifs', {"q": "cats", "limit": "1"})
+  .then((response) => {
+    response.data.forEach((gifObject) => {
+      console.log(gifObject.url)
+    })
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  
   function addImageToApp(index) {
     var imgType = document.getElementById("hidden-type" + index).value;
     var imgValue = document.getElementById("hidden-val" + index).value;
