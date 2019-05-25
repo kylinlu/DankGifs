dd6b4794e7ee7c180766b2d4063a36ed8b3567d8
䂊汐 <28.juxi.lu@gmail.com>
2019-05-25 19:38:54 -0400

    returns giphy gif of clarifai's top 3 guesses

--- server/src/predict.js
+++ server/src/predict.js
@@ -6,27 +6,25 @@ client = GphApiClient(process.env.GIPHY_KEY);
 // Instantiate a new Clarifai app by passing in your API key.
 const app = new Clarifai.App({apiKey: process.env.CLARIFAI_KEY});
 
-var guess
-
 // Predict the contents of an image by passing in a URL.
-app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg')
+app.models.predict(Clarifai.GENERAL_MODEL, 'https://assetsds.cdnedge.bluemix.net/sites/default/files/styles/very_big_1/public/feature/images/why_you_should_go_mountain_climbing_1.jpg?itok=wYnlNZir')
   .then(response => {
-    guess= JSON.stringify(response.outputs[0]["data"]["concepts"][0]["name"]);
-    guessAccuracy = JSON.stringify(response.outputs[0]["data"]["concepts"][0]["value"]);
+    var guess1 = JSON.stringify(response.outputs[0]["data"]["concepts"][0]["name"]);
+    var guess2 = JSON.stringify(response.outputs[0]["data"]["concepts"][1]["name"]);
+    var guess3 = JSON.stringify(response.outputs[0]["data"]["concepts"][2]["name"]);
+    console.log(guess1)
+    console.log(guess2)
+    console.log(guess3)
+    client.search('gifs', {"q": `${guess1} ${guess2} ${guess3}`, "limit": "1"})
+    .then((response) => {
+      response.data.forEach((gifObject) => {
+        console.log(gifObject.url)
+      })
+    })
   })
   .catch(err => {
     console.log(err);
   });
-  
-  client.search('gifs', {"q": "cats", "limit": "1"})
-  .then((response) => {
-    response.data.forEach((gifObject) => {
-      console.log(gifObject.url)
-    })
-  })
-  .catch((err) => {
-    console.log(err);
-  })
   
   function addImageToApp(index) {
     var imgType = document.getElementById("hidden-type" + index).value;
